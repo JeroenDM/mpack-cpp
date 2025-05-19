020e03da724f517359af70b3f4b0194776892136
jeroendm <jeroendemaeyer@live.be>
2025-05-19 09:37:40 +0200

    addapt default reader to use node API

--- include/mpack_cpp/mpack_reader.hpp
+++ include/mpack_cpp/mpack_reader.hpp
@@ -19,13 +19,14 @@ namespace mpack_cpp {
 namespace internal {
 
 template <typename T, typename... Args>
-void read_and_assign(std::variant<Args...>& variant, T (*reader_func)(mpack_reader_t*),
-                     mpack_reader_t* reader) {
-    T value = reader_func(reader);
+void read_and_assign(std::variant<Args...>& variant, T (*node_func)(mpack_node_t),
+                     mpack_node_t node) {
+    T value = node_func(node);
     if constexpr ((std::is_same_v<T, Args> || ...)) {
         variant = value;
     } else {
-        mpack_reader_flag_error(reader, mpack_error_unsupported);
+        assert(false);
+        // mpack_reader_flag_error(node, mpack_error_unsupported);
     }
 }
 
@@ -42,72 +43,72 @@ void read_and_assign(std::variant<Args...>& variant, T (*reader_func)(mpack_read
  *
  */
 struct ReadVisitor {
-    mpack_reader_t& reader;
+    mpack_node_t node;
 
-    void operator()(bool& out) { out = mpack_expect_bool(&reader); }
+    void operator()(bool& out) { out = mpack_node_bool(node); }
 
-    void operator()(float& out) { out = mpack_expect_float(&reader); }
-    void operator()(double& out) { out = mpack_expect_double(&reader); }
+    void operator()(float& out) { out = mpack_node_float(node); }
+    void operator()(double& out) { out = mpack_node_double(node); }
 
     // Should we use the generic mpack_expect_int here to make it more flexible?
-    void operator()(std::uint8_t& out) { out = mpack_expect_u8(&reader); }
-    void operator()(std::uint16_t& out) { out = mpack_expect_u16(&reader); }
-    void operator()(std::uint32_t& out) { out = mpack_expect_u32(&reader); }
-    void operator()(std::uint64_t& out) { out = mpack_expect_u64(&reader); }
+    void operator()(std::uint8_t& out) { out = mpack_node_u8(node); }
+    void operator()(std::uint16_t& out) { out = mpack_node_u16(node); }
+    void operator()(std::uint32_t& out) { out = mpack_node_u32(node); }
+    void operator()(std::uint64_t& out) { out = mpack_node_u64(node); }
 
-    void operator()(std::int8_t& out) { out = mpack_expect_i8(&reader); }
-    void operator()(std::int16_t& out) { out = mpack_expect_i16(&reader); }
-    void operator()(std::int32_t& out) { out = mpack_expect_i32(&reader); }
-    void operator()(std::int64_t& out) { out = mpack_expect_i64(&reader); }
+    void operator()(std::int8_t& out) { out = mpack_node_i8(node); }
+    void operator()(std::int16_t& out) { out = mpack_node_i16(node); }
+    void operator()(std::int32_t& out) { out = mpack_node_i32(node); }
+    void operator()(std::int64_t& out) { out = mpack_node_i64(node); }
 
     /** Decode any kind of string with support for custom allocators
      * (e.g std::pmr::vector).
      */
     template <typename CharT, typename Traits, typename Allocator>
-    void operator()(std::basic_string<CharT, Traits, Allocator>& value) {
-        uint32_t length = mpack_expect_str(&reader);
-        value.resize(static_cast<std::size_t>(length));
-        mpack_read_cstr(&reader, value.data(), 100, value.size());
-        mpack_done_str(&reader);
+    void operator()(std::basic_string<CharT, Traits, Allocator>& out) {
+        out.resize(mpack_node_strlen(node) + 1);
+        mpack_node_copy_cstr(node, out.data(), out.size());
+        out.resize(mpack_node_strlen(node));
     }
 
     /** Decode any kind of vector with support for custom allocators
-     * (e.g. std::pmr::string),
+     * (e.g. std::pmr::vector),
      */
-    template <typename ElemT, typename Allocator>
-    void operator()(std::vector<ElemT, Allocator>& vec) {
-        std::size_t count = mpack_expect_array_max(&reader, 100);
-        vec.resize(count);
-        for (std::size_t i{0}; i < count; ++i) {
-            (*this)(vec[i]);
+    template <typename T, typename Allocator>
+    void operator()(std::vector<T, Allocator>& out) {
+        out.resize(mpack_node_array_length(node));
+        for (std::size_t i{0}; i < out.size(); ++i) {
+            auto nested_node = mpack_node_array_at(node, i);
+            ReadVisitor{nested_node}(out[i]);
         }
-        mpack_done_array(&reader);
     }
 
     /** Decode a pair from a fixed length array, two element, MessagePack array. */
     template <typename FirstT, typename SecondT>
     void operator()(std::pair<FirstT, SecondT>& pair) {
-        mpack_expect_array_match(&reader, 2);
-        (*this)(pair.first);
-        (*this)(pair.second);
-        mpack_done_array(&reader);
+        if (2 == mpack_node_array_length(node)) {
+            ReadVisitor{mpack_node_array_at(node, 0)}(pair.first);
+            ReadVisitor{mpack_node_array_at(node, 1)}(pair.second);
+        } else {
+            assert(false);
+            // mpack_node_flag_error(mpack_error_type);
+        }
     }
 
     template <typename... Args>
     void operator()(std::variant<Args...>& variant) {
-        auto tag = mpack_peek_tag(&reader);
-        switch (tag.type) {
+        switch (mpack_node_type(node)) {
             case mpack_type_bool:
-                read_and_assign<bool>(variant, mpack_expect_bool, &reader);
+                read_and_assign<bool>(variant, mpack_node_bool, node);
                 break;
             case mpack_type_double:
-                read_and_assign<double>(variant, mpack_expect_double, &reader);
+                read_and_assign<double>(variant, mpack_node_double, node);
                 break;
             case mpack_type_uint:
-                read_and_assign<uint64_t>(variant, mpack_expect_u64, &reader);
+                read_and_assign<uint64_t>(variant, mpack_node_u64, node);
                 break;
             default:
-                mpack_reader_flag_error(&reader, mpack_error_unsupported);
+                mpack_node_flag_error(node, mpack_error_unsupported);
         }
     }
     /**
@@ -126,11 +127,10 @@ struct ReadVisitor {
     // template<typename T, std::enable_if<has_from_message_pack_v<T>, int> = 0>
     template <typename T>
     void operator()(T& value) {
-        std::size_t n = mpack_expect_map_max(&reader, 30);
+        std::size_t n = mpack_node_map_count(node);
         if (n > 0) {
-            value.from_message_pack(reader);
+            value.from_message_pack(node);
         }
-        mpack_done_map(&reader);
     }
 };
 
@@ -143,73 +143,76 @@ struct ReadVisitor {
  *   - Extension types: `ReadExtField`
  */
 template <typename T>
-void ReadField(mpack_reader_t& reader, const char* key, T&& value) {
-    mpack_expect_cstr_match(&reader, key);
-    internal::ReadVisitor{reader}(std::forward<T>(value));
+void ReadField(mpack_node_t node, const char* key, T&& out) {
+    auto value_node = mpack_node_map_cstr(node, key);
+    internal::ReadVisitor{value_node}(std::forward<T>(out));
 }
 
-template <std::size_t N>
-void ReadExtField(mpack_reader_t& reader, const char* key, std::int8_t& type,
-                  std::array<char, N>& data) {
-    mpack_expect_cstr_match(&reader, key);
-    auto n =
-        mpack_expect_ext_max(&reader, &type, static_cast<std::uint32_t>(data.size()));
-    if (n != data.size()) {
-        mpack_reader_flag_error(&reader, mpack_error_data);
-    } else {
-        mpack_read_bytes(&reader, data.data(), data.size());
-        mpack_done_ext(&reader);
-    }
-}
+// template <std::size_t N>
+// void ReadExtField(mpack_reader_t& reader, const char* key, std::int8_t& type,
+//                   std::array<char, N>& data) {
+//     mpack_expect_cstr_match(&reader, key);
+//     auto n =
+//         mpack_expect_ext_max(&reader, &type, static_cast<std::uint32_t>(data.size()));
+//     if (n != data.size()) {
+//         mpack_reader_flag_error(&reader, mpack_error_data);
+//     } else {
+//         mpack_read_bytes(&reader, data.data(), data.size());
+//         mpack_done_ext(&reader);
+//     }
+// }
 
-namespace internal {
-inline bool IsFixStr(const char c) {
-    // Check for pattern 101xxxxx.
-    return (static_cast<std::uint8_t>(c) >> 5) == 5;
-}
+// namespace internal {
+// inline bool IsFixStr(const char c) {
+//     // Check for pattern 101xxxxx.
+//     return (static_cast<std::uint8_t>(c) >> 5) == 5;
+// }
 
-inline std::uint8_t GetFixStrLength(const char c) {
-    // Extract last 5 bits as uint8.
-    return static_cast<std::uint8_t>(c) & static_cast<uint8_t>(~0xe0);
-}
-}  // namespace internal
+// inline std::uint8_t GetFixStrLength(const char c) {
+//     // Extract last 5 bits as uint8.
+//     return static_cast<std::uint8_t>(c) & static_cast<uint8_t>(~0xe0);
+// }
+// }  // namespace internal
 
-/** Decode optional fields.
- *
- * @note: currently the maximum length for the key string is 31 bytes,
- * because it is hardcoded to the the MesssagePack type 'fixstr'.
- */
-template <typename T>
-void ReadOptionalField(mpack_reader_t& reader, const char* key, T&& value) {
-    value = std::nullopt;
-    if (!internal::IsFixStr(reader.data[0])) {
-        // TODO(jeroendm) support arbitrary sized strings.
-        // TODO(jeroendm) we cannot set the error flag here
-        // because then optiona_at_end case would fail.
-        // This is because the if check here is also used to check
-        // if the optional at the end if there.
-        // Bad design, complete redesign needed to fix it.
-        // mpack_reader_flag_error(&reader, mpack_error_type);
-        return;
-    }
+// /** Decode optional fields.
+//  *
+//  * @note: currently the maximum length for the key string is 31 bytes,
+//  * because it is hardcoded to the the MesssagePack type 'fixstr'.
+//  */
+// template <typename T>
+// void ReadOptionalField(mpack_reader_t& reader, const char* key, T&& value) {
+//     value = std::nullopt;
+//     if (!internal::IsFixStr(reader.data[0])) {
+//         // TODO(jeroendm) support arbitrary sized strings.
+//         // TODO(jeroendm) we cannot set the error flag here
+//         // because then optiona_at_end case would fail.
+//         // This is because the if check here is also used to check
+//         // if the optional at the end if there.
+//         // Bad design, complete redesign needed to fix it.
+//         // mpack_reader_flag_error(&reader, mpack_error_type);
+//         return;
+//     }
 
-    auto length = internal::GetFixStrLength(reader.data[0]);
-    const std::string key_s(key, strlen(key));
-    const std::string next_key_s(reader.data + 1, length);
-    if (key_s != next_key_s) {
-        return;
-    }
+//     auto length = internal::GetFixStrLength(reader.data[0]);
+//     const std::string key_s(key, strlen(key));
+//     const std::string next_key_s(reader.data + 1, length);
+//     if (key_s != next_key_s) {
+//         return;
+//     }
 
-    value.emplace();
-    ReadField(reader, key, value.value());
-}
+//     value.emplace();
+//     ReadField(reader, key, value.value());
+// }
 
 template <typename T>
 bool ReadFromMsgPack(T& data, const char* buffer_start, std::size_t msg_size) {
-    mpack_reader_t reader;
-    mpack_reader_init_data(&reader, buffer_start, msg_size);
-    internal::ReadVisitor{reader}(data);
-    auto err = mpack_reader_destroy(&reader);
+    mpack_tree_t tree;
+    mpack_tree_init_data(&tree, buffer_start, msg_size);
+    // mpack_tree_init_pool(&tree, buffer.data(), n, pool.data(), pool.size());
+    mpack_tree_parse(&tree);
+    mpack_node_t root = mpack_tree_root(&tree);
+    internal::ReadVisitor{root}(data);
+    auto err = mpack_tree_destroy(&tree);
     if (err != mpack_ok) {
         fprintf(stderr, "An error occurred decoding the data!\n");
         fprintf(stderr, "%s!\n", mpack_error_to_string(err));
